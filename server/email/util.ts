be73b7231263b0d7684b180425850ad9749f8c3e
Adam Janicki <adamjanicki2@gmail.com>
2022-11-26 11:46:59 -0500

    fix email bug

--- server/email/util.ts
+++ server/email/util.ts
@@ -23,38 +23,6 @@ const sendEmail = async (
     auth: { user: email, pass: password },
   });
 
-  // await new Promise((resolve, reject) => {
-  //   // verify connection configuration
-  //   transporter.verify(function (error, success) {
-  //     if (error) {
-  //       console.log(error);
-  //       reject(error);
-  //     } else {
-  //       console.log("Server is ready to take our messages");
-  //       resolve(success);
-  //     }
-  //   });
-  // });
-  // await new Promise((resolve, reject) => {
-  //   // send mail
-  //   transporter.sendMail(
-  //     {
-  //       from: `Producktive Duck <${email}>`,
-  //       to: recipient,
-  //       subject,
-  //       html,
-  //     },
-  //     (err, info) => {
-  //       if (err) {
-  //         console.error(err);
-  //         reject(err);
-  //       } else {
-  //         console.log(info);
-  //         resolve(info);
-  //       }
-  //     }
-  //   );
-  // });
   const mail = await transporter.sendMail({
     from: `Producktive Duck <${email}>`,
     to: recipient,
