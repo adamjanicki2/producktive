c3e0676a76391396ffc30b19877a5e9f3db132df
Adam Janicki <adamjanicki2@gmail.com>
2022-11-26 11:21:57 -0500

    fix email bug

--- server/email/util.ts
+++ server/email/util.ts
@@ -27,8 +27,10 @@ const sendEmail = async (
     // verify connection configuration
     transporter.verify(function (error, success) {
       if (error) {
+        console.log(error);
         reject(error);
       } else {
+        console.log("Server is ready to take our messages");
         resolve(success);
       }
     });
