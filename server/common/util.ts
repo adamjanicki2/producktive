4a40d748b42a2b09a9825d9b83f191ddf563e5ca
Adam Janicki <adamjanicki2@gmail.com>
2022-12-01 10:31:10 -0500

    fixes type errors

--- server/common/util.ts
+++ server/common/util.ts
@@ -1,27 +1,31 @@
+import type { Task } from "../task/model";
+
+// map difficulty to coin amount
+const DIFFICULTY_TO_REWARD = {
+  easy: 10,
+  medium: 20,
+  hard: 30,
+} as const;
+
 /**
  * Coin Algorithm
- * 
+ *
  * @param {Date} due - date task due
  * @param {Date} completed - date task completed
- * @param {string} difficulty - "easy" | "medium" | "hard" task difficulty
- * 
+ * @param {Task["difficulty"]} difficulty - "easy" | "medium" | "hard" task difficulty
+ *
  * @return {number} - The coins the user earned based on difficulty and completion relative to due date
  */
-function coins(due: Date, completed: Date, difficulty: string) {
-    const base = (difficulty == 'hard')? 30: (difficulty == 'medium')? 20: (difficulty == 'easy')? 10: 0;
-     
-    const diff = due.valueOf() - completed.valueOf();
-    const diffDays = Math.ceil(diff / (1000 * 3600 * 24)); 
-    const punctuality = 1.1**diffDays;
-    
-    const coins = Math.round(base * punctuality);
-    return coins;
+export function coins(
+  due: Date,
+  completed: Date,
+  difficulty: Task["difficulty"]
+): number {
+  // time difference in days
+  // negative if completed late
+  const timeDelta = (due.valueOf() - completed.valueOf()) / (1000 * 3600 * 24);
+  const onTimeFactor = 1.1 ** timeDelta;
+  const difficultyScale = DIFFICULTY_TO_REWARD[difficulty];
+  const coins = Math.round(difficultyScale * onTimeFactor);
+  return coins;
 }
-
-
-const completed = new Date('2022-12-20');
-const due = new Date('2022-12-23');
-
-console.log(`hard: ${coins(due, completed, 'hard')}`);
-console.log(`medium: ${coins(due, completed, 'medium')}`);
-console.log(`easy: ${coins(due, completed, 'easy')}`);
