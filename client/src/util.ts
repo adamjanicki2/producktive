3c5d0a2d458fc1d72cb53641537369300c9c0f1c
Madison Wong <madiwong@mit.edu>
2022-12-06 10:14:42 -0500

    fixed spelling error in monthly

--- client/src/util.ts
+++ client/src/util.ts
@@ -64,7 +64,7 @@ export type User = {
   _id: string;
   email: string;
   username: string;
-  notifPeriod: "daily" | "weekly" | "montly" | "none";
+  notifPeriod: "daily" | "weekly" | "monthly" | "none";
   coins: number;
 };
 
