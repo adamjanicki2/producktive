4bae932ffc5b628bbbdd42317b1942f86a4b1f01
Adam Janicki <adamjanicki2@gmail.com>
2022-12-10 13:43:27 -0500

    task fixes

--- client/src/components/pages/HomePage.tsx
+++ client/src/components/pages/HomePage.tsx
@@ -2,9 +2,9 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { get, Pet, Task, User, del, patch } from "../../util";
+import { get, Pet, User } from "../../util";
 import Duck from "../modules/Duck";
-import { TaskNode } from "./ListPage";
+import ListSection from "../modules/ListSection";
 
 const Home = ({
   user,
@@ -14,29 +14,15 @@ const Home = ({
   updateUser: (user: User) => void;
 }) => {
   const [ducks, setDucks] = React.useState<Pet[]>([]);
-  const [tasks, setTasks] = React.useState<Task[]>([]);
   const [search, setSearch] = React.useState("");
 
   useEffect(() => {
     const setup = async () => {
       const pets = await get("/api/pets/all");
       setDucks(pets);
-      const tasks = await get("/api/tasks/today");
-      !tasks?.error && setTasks(tasks);
     };
     setup();
   }, []);
-
-  const deleteTask = async (id: string) => {
-    if (window.confirm("Are you sure you want to delete this task?")) {
-      const task = await del(`/api/tasks/${id}`);
-      if (task?.error) {
-        return window.alert(task.error);
-      } else {
-        setTasks(tasks.filter((t) => t._id !== id));
-      }
-    }
-  };
 
   const searchDucks = async () => {
     const endpoint = `/api/pets/${search || "all"}`;
@@ -50,28 +36,6 @@ const Home = ({
     setSearch("");
   };
 
-  const completeTask = async (id: string) => {
-    const res = await patch(`/api/tasks/complete/${id}`);
-    if (res?.error) {
-      return window.alert(res.error);
-    } else {
-      setTasks(
-        tasks.map((t) => (t._id === id ? { ...t, completed: true } : t))
-      );
-      const { coinsDelta } = res;
-      updateUser({ ...user, coins: user.coins + coinsDelta });
-    }
-  };
-
-  const editTask = async (id: string, content: string) => {
-    const res = await patch(`/api/tasks/${id}`, { content });
-    if (res?.error) {
-      return window.alert(res.error);
-    } else {
-      setTasks(tasks.map((t) => (t._id === id ? { ...t, content } : t)));
-    }
-  };
-
   return user && ducks.length > 0 ? (
     <div className="flex flex-column primary-text">
       <h1 className="tc f-subheadline ma0 pa0">Home</h1>
@@ -82,20 +46,12 @@ const Home = ({
         How does Producktive Work?
       </Link>
       <h1 className="tc f1">Your Upcoming Tasks</h1>
-      {!tasks.length && (
-        <h2 className="tc">You do not have any tasks for today!</h2>
-      )}
-      <div className="flex flex-column w-70 m-auto">
-        {tasks.map((task, index) => (
-          <TaskNode
-            key={`task${index}`}
-            task={task}
-            deleteTask={deleteTask}
-            completeTask={completeTask}
-            editTask={editTask}
-          />
-        ))}
-      </div>
+      <ListSection
+        user={user}
+        updateUser={updateUser}
+        endpoint="/api/tasks/today"
+        noTasksMessage="You have no tasks today!"
+      />
       <hr className="near-black b--near-black ba bw1 w-80" />
       <h1 className="tc f1 primary-text">Check out everyone's ducks!</h1>
       <div className="flex flex-row items-center m-auto">
--- client/src/components/pages/ListViewPage.tsx
+++ client/src/components/pages/ListViewPage.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { User } from "../../util";
+import ListSection from "../modules/ListSection";
 import SideNav from "../modules/SideNav";
 
-const View = () => {
+const View = ({
+  user,
+  updateUser,
+}: {
+  user?: User;
+  updateUser: (user: User) => void;
+}) => {
   return (
     <div className="flex flex-row w-100">
       <SideNav />
       <div className="flex flex-column w-80">
-        <h1 className="tc f-subheadline ma0 pa0 ">Your Lists</h1>
+        <h1 className="tc f-subheadline ma0 pa0 ">Upcoming</h1>
+        <ListSection
+          endpoint="/api/tasks/today"
+          user={user}
+          updateUser={updateUser}
+          noTasksMessage="You have no tasks today!"
+        />
       </div>
     </div>
   );
