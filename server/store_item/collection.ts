0e3e61274d3dfac6ef4e0af5ee0b3dba78f78c5f
Adam Janicki <adamjanicki2@gmail.com>
2022-12-10 12:44:52 -0500

    coin fix

--- server/store_item/collection.ts
+++ server/store_item/collection.ts
@@ -4,9 +4,9 @@ import ItemModel from "./model";
 import UserCollection from "../user/collection";
 
 const PRICES = {
-  "food": 15,
-  "beak": 200,
-  "duck": 250,
+  food: 15,
+  beak: 200,
+  duck: 250,
 } as const;
 
 class ItemCollection {
@@ -23,10 +23,13 @@ class ItemCollection {
     userId: Types.ObjectId | string,
     type: string,
     identifier: string,
-    properties: Record<string, string>
+    properties: Record<string, string>,
+    free = false
   ): Promise<HydratedDocument<Item>> {
     const purchased = new ItemModel({ userId, type, identifier, properties });
-    await UserCollection.updateCoins(userId, -1*PRICES[type]); //negative to lose coins
+    if (!free) {
+      await UserCollection.updateCoins(userId, -1 * PRICES[type]); //negative to lose coins
+    }
     await purchased.save();
     return purchased;
   }
@@ -46,9 +49,7 @@ class ItemCollection {
   /**
    * Delete item once used (for food)
    */
-  static async deleteOne(
-    itemId: Types.ObjectId | string
-  ): Promise<boolean> {
+  static async deleteOne(itemId: Types.ObjectId | string): Promise<boolean> {
     const item = await ItemModel.deleteOne({ _id: itemId });
     return item !== null;
   }
@@ -60,7 +61,10 @@ class ItemCollection {
     userId: Types.ObjectId | string,
     identifier: string
   ): Promise<HydratedDocument<Item> | null> {
-    const item = await ItemModel.findOne({userId: userId, identifier: identifier});
+    const item = await ItemModel.findOne({
+      userId: userId,
+      identifier: identifier,
+    });
     return item;
   }
 }
